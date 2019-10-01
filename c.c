5e54f3bba0fbb01ab0330d919dc210097b2eebf4
Abhinav Mittra <abhinavmittra@rediffmail.com>
2019-10-01 22:12:32 +0530

    Update c.c

--- c.c
+++ c.c
@@ -1,4 +1,5 @@
 #include <iostream>
+#include<string>
 using namespace std;
 
 string compress(string text)
@@ -6,9 +7,9 @@ string compress(string text)
     string ct="";
     int i,j,count=0;
     
-    for(i=0;i<text.length();i++)
+    for(i=0;i<strlen(text);i++)
    { count=1;
-      for(j=i+1;j<text.length();j++)
+      for(j=i+1;j<strlen(text);j++)
      {
         if(text[i]==text[j])
         count++;
@@ -16,7 +17,7 @@ string compress(string text)
         break;
      }
      ct+=text[i]+to_string(count);
-     i=i+count;
+     i=i+count-1;
    }
    return ct
        
